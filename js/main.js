ac5398b5918471a0320476cfd0fc2847a0157003
quocbao <johndoe@example.comgit>
2021-07-25 23:34:03 +0700

    Add cart , miniCart , quickview , ...

--- js/main.js
+++ js/main.js
@@ -1,175 +1,330 @@
-$(function () {
+$(document).ready(function () {
 
-    const products = [{
-            id: 1,
-            name: 'Women’s classic glasses',
-            price: '$80.00',
-            img: '../img/product/20-copy-600x745.jpg',
-            img1: '../img/product/20_1-copy-600x745.jpg',
-            isSale: false,
-            priceSale: '$'
-        },
-        {
-            id: 2,
-            name: 'Daily Ritual Women’s',
-            price: '$80.00',
-            img: '../img/product/2_3-copy-600x745.jpg',
-            img1: '../img/product/2_4-copy-600x745.jpg',
-            isSale: true,
-            priceSale: '$65.00'
-        },
-        {
-            id: 3,
-            name: 'Cotton Fleece Jogging',
-            price: '$39.00',
-            img: '../img/product/10_1-copy-600x745.jpg',
-            img1: '../img/product/10_2-copy-600x745.jpg',
-            isSale: false,
-            priceSale: '$'
-        },
-        {
-            id: 4,
-            name: 'Sweeper and Funnel',
-            price: '$80.00',
-            img: '../img/product/9-copy-600x745.jpg',
-            img1: '../img/product/9_2-copy-1-600x745.jpg',
-            isSale: false,
-            priceSale: '$'
-        },
-        {
-            id: 5,
-            name: 'Mogens Koch Bookcase',
-            price: '$28.00',
-            img: '../img/product/8_1-copy-600x745.jpg',
-            img1: '../img/product/8_2-copy-600x745.jpg',
-            isSale: false,
-            priceSale: '$'
-        },
-        {
-            id: 6,
-            name: 'Unisex Fashion Show',
-            price: '$39.00',
-            img: '../img/product/1_1-600x745.jpg',
-            img1: '../img/product/2_4-copy-600x745.jpg',
-            isSale: false,
-            priceSale: '$'
-        }, {
-            id: 7,
-            name: 'Neocroc backpack in canvas',
-            price: '$259.00',
-            img: '../img/product/17_1-copy-600x745.jpg',
-            img1: '../img/product/17-copy-600x745.jpg',
-            isSale: true,
-            priceSale: '$220.00'
-        }, {
-            id: 8,
-            name: 'Small Zip Tote Bag',
-            price: '$80.00',
-            img: '../img/product/11_1-copy-600x745.jpg',
-            img1: '../img/product/20_1-copy-600x745.jpg',
-            isSale: false,
-            priceSale: '$'
-        }
-    ]
+    var miniCart = parseLocal('dataCart') || []
+    var wishList = parseLocal('WishListProduct') || []
+    const products = getAllItemProduct()
+    $(function () {
 
-    const blogs = {
+        // Load data storage
+        renderMiniCart(miniCart)
+        changeStatus(miniCart.length)
+        renderCart(miniCart)
+        //whist list 
+        renderWishList(wishList)
+        $('.addWishlist .before').html(wishList.length)
 
-        items: [{
-            id: "b1",
-            img: '../img/banner/8-500x321.jpg',
-            head: 'Note to wear well in the workplace',
-            des: 'A being too greedy for accessories to Ms. B who looks too old because of the carefulness and dullness of …',
-        }, {
-            id: "b2",
-            img: '../img/banner/7-500x321.jpg',
-            head: '5 ways coordination for puff sleeves',
-            des: 'You are a girl who loves gentle, feminine style, “dirt bread” and you love gentle pastel tones and feminine designs. …'
-        }, {
-            id: "b3",
-            img: '../img/banner/6-500x321.jpg',
-            head: 'How to mix with tunic shoulders',
-            des: 'How do you wear a shirt on your shoulders every day? Or slip off and maybe not comfortable with that …'
-        }, {
-            id: "b4",
-            img: '../img/banner/5-500x321.jpg',
-            head: '7 ways to wear the shoulder blouse',
-            des: 'Shorts, skirts, earrings, hats, … are one of the ways to wear a shoulder- length shirt that any girl can …'
-        }],
-        renderBlog: function () {
+        // Scroll window
+        $(document).scroll(function () {
+            const currentPos = $(this).scrollTop()
+            $('.main-header').toggleClass('active', currentPos > 150);
+            $('.backtotop').toggleClass('active', currentPos > 500);
 
-            $('#add-items-blog').empty();
-            this.items.forEach(val => {
-                const htmls = `
-                <div class="owl-item">
-                    <div class="item">
-                        <a href="#" class="link">
-                            <div class="img">
-                                <img src="${val.img}" alt="">
-                            </div>
-                            <p>Admin. <span class="date">June 10, 2021</span></p>
-                            <h2 class="heading">${val.head}</h2>
-                        </a>
-                        <p class="des">${val.des}</p>
-                        <a href="#" class="btn">Read More</a>
-                    </div>
-                </div>
-                `
-                $('#add-items-blog').append(htmls)
-            })
-        },
-        start: function () {
+        })
 
-            this.renderBlog()
+        // when search click
+        $('#search').on('click', function (e) {
+
+            e.preventDefault()
+            $('.form-search').addClass('active')
+
+        })
+
+        $('.form-search .close').click(function (e) {
+
+            e.preventDefault()
+            $('.form-search').removeClass('active')
+
+        })
+
+        // when user click on cart 
+        $('.cart-item').on('click', function (e) {
+
+            e.preventDefault()
+            $('.cart').addClass('active')
+
+        })
+
+        $(document).click(function (e) {
+
+            const target = e.target
+            // when you click somewhere is not a minicart
+            if ((target.classList.contains('cart') && target.classList.contains('active')) || target.classList.contains('close')) {
+                $('.cart').removeClass('active')
+            }
+        })
+
+        // when you click add ele to cart 
+        $(document).on('click', '.addcartitem', function (e) {
+
+            const target = e.target
+            const id = +target.dataset.id
+            const idxSame = miniCart.findIndex(val => val.id == id)
+
+            // have ID 
+            if (idxSame != -1) {
+
+                miniCart[idxSame].quantity = ++miniCart[idxSame].quantity
+
+            } else {
+
+                // get a product = id and push miniCart
+                const ele = products.find(val => val.id == id)
+                miniCart.push({
+                    ...ele,
+                    quantity: 1
+                })
+            }
+
+            renderMiniCart(miniCart)
+            changeStatus(miniCart.length)
+            $('.cart').addClass('active')
+            setLocal(miniCart, 'dataCart')
+
+        });
+
+        // when you remove ele from minicart
+        $(document).on('click', '.erase', function (e) {
+            const target = e.target
+            // Get ID 
+            const id = target.parentNode.dataset.id
+            removeItemMiniCart(id)
+            changeStatus(miniCart.length)
+            renderMiniCart(miniCart)
+        });
+
+        // when you remove ele from cart
+        $(document).on('click', '#addToCard .erase', function (e) {
+
+            const id = $(this).parents('.product-item').data("id")
+            removeItemMiniCart(id)
+
+        });
+
+        // add wishlist
+        $(document).on('click', '.action .wishlist', function (e) {
+
+            const id = $(this).parents('.product').data('id')
+            const ele = products.find(val => val.id == id)
+            wishList.push(ele)
+            $('.addWishlist .before').html(wishList.length)
+            setLocal(wishList, 'WishListProduct')
+
+        })
+
+        $(document).on('click', '#addToWish .erase', function (e) {
+
+            const id = $(this).parents('.product-item').data("id")
+            removeWishItem(id)
+            $('.addWishlist .before').html(wishList.length)
+
+        });
+
+        // viewDetails Quickview
+        $(document).on('click', '.action .detail', function (e) {
+
+            const id = $(this).parents('.product').data('id')
+            const ele = products.find(val => val.id == id)
+            $('.quickview').addClass('active')
+            renderQuickView(ele)
+
+        })
+
+        $('.modal .close').on('click', function (e) {
+
+            $('.quickview').removeClass('active')
+
+        })
+
+    });
+
+    function removeWishItem(id) {
+
+        wishList = wishList.filter(val => val.id != +id)
+        renderWishList(wishList)
+        setLocal(wishList, 'WishListProduct')
+
+    }
+
+    function removeItemMiniCart(id) {
+        // remove ele in miniCart
+        miniCart = miniCart.filter(val => val.id != +id)
+        renderCart(miniCart)
+        // Render again
+        setLocal(miniCart, 'dataCart')
+
+    }
+
+    // change status of minicart
+    function changeStatus(length) {
+
+        if (length > 0) {
+
+            $('.mini-cart').addClass('active')
+            $('.no-product').addClass('active')
+
+        } else {
+
+            $('.no-product').removeClass('active')
+            $('.mini-cart').removeClass('active')
 
         }
     }
 
-    blogs.start()
+    function renderMiniCart(datas) {
 
-    // Scroll window
-    $(document).scroll(function () {
-        const currentPos = $(this).scrollTop()
-        $('.main-header').toggleClass('active', currentPos > 150);
-        $('.backtotop').toggleClass('active', currentPos > 500);
+        const mini_cart = $('#mini-cart-product')
+        const subTotal = $('.total .sub-total')
+        const amount = $('.cart-item .before')
+        mini_cart.empty()
 
-    })
+        const total = datas.reduce((acc, val) => {
+            const htmls = `
+            <li class="d-flex" data-id=${val.id}>
+                <a href="#" class="img ">
+                    <img src="${val.img}" alt="">
+                </a>
+                <div class="quantity">
+                    <a href="#">${val.name}</a>
+                    <p> <span class="countEle"> ${val.quantity} </span> × $${val.price}</p>
+                </div>
+                <span class ="erase">&times;</span>
+            </li>
+        `
+            mini_cart.append(htmls);
+            return (+val.price * val.quantity) + acc
+        }, 0)
 
+        const length = datas.reduce((acc, val) => {
+            return acc + val.quantity
+        }, 0)
+        // render sum of ele
+        amount.html(length)
+        // render subtotal in minicart
+        subTotal.html(`$${total}.00`)
+    }
 
-    $('document').click(function (e) {
-        e.preventDefault();
+    function renderCart(datas) {
 
+        const list = $('#addToCard')
+        list.empty()
+        const total = datas.reduce((acc, val) => {
+            const htmls = `
+                <tr class ="product-item" data-id="${val.id}">
+                    <td>
+                        <div class="erase">
+                            &times;
+                        </div>
+                    </td>
+                    <td>
+                        <div class="img">
+                            <img src="${val.img}" alt="">
+                        </div>
+                    </td>
+                    <td>
+                        <div class="heading-product">
+                            ${val.name}
+                        </div>
+                    </td>
+                    <td>
+                        $${val.price}
+                    </td>
+                    <td>
+                        <div class="control d-flex a-center">
+                            <a href=""><i class="bi bi-plus"></i></a>
+                            <input type="text" value="${val.quantity}">
+                            <a href=""><i class="bi bi-dash"></i></a>
+                        </div>
+                    </td>
+                    <td>${val.price}</td>
+                </tr>  
+            `
+            list.append(htmls)
+            return (val.quantity * val.price) + acc
+        }, 0)
 
+        $('.sub-totals .subtotal').html(`$${total}.00`)
+        $('.total .total-product').html(`$${total}.00`)
+    }
 
-    });
+    function renderWishList(datas) {
 
-    // when search click
-    $('#search').on('click', function (e) {
+        const list = $('#addToWish')
+        list.empty()
+        datas.map(val => {
+            const htmls = `
+                <tr class ="product-item" data-id="${val.id}">
+                    <td>
+                        <div class="erase">
+                            &times;
+                        </div>
+                    </td>
+                    <td>
+                        <div class="img">
+                            <img src="${val.img}" alt="">
+                        </div>
+                    </td>
+                    <td>
+                        <div class="heading-product">
+                            ${val.name}
+                        </div>
+                    </td>
+                    <td>
+                        $${val.price}
+                    </td>
+                    <td>
+                        In Stock
+                    </td>
+                    <td>
+                        <button class ="addcartitem" data-id="${val.id}">Add to card</button>
+                    </td>
+                </tr>  
+            `
+            list.append(htmls)
+        }, 0)
 
-        e.preventDefault()
-        $('.form-search').addClass('active')
+    }
 
-    })
+    function setLocal(datas, name) {
 
-    $('.form-search .close').click(function (e) {
+        return localStorage.setItem(`${name}`, JSON.stringify(datas))
 
-        e.preventDefault()
-        $('.form-search').removeClass('active')
+    }
 
-    })
+    function parseLocal(name) {
 
-    // when user click on cart 
-    $('.cart-item').on('click', function (e) {
+        const data = localStorage.getItem(name);
+        return JSON.parse(data)
 
-        e.preventDefault()
-        $('.cart').addClass('active')
+    }
 
-    })
+    function renderQuickView(datas) {
 
-    $('.cart .products-cart .close').on('click', function (e) {
-
-        e.preventDefault()
-        $('.cart').removeClass('active')
-
-    })
+        $('.modal .content').html(`
+            <a href="#">${datas.name}</a>
+            <div class="star">
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i> (1 customer review)
+            </div>
+            <p class="price">$${datas.price}</p>
+            <p class="des">
+                .Tailored line. Wool mix fabric. Long design. Long buttoned sleeve. Lapel with notch. Back slit.
+                Two pockets with flaps on the front. Button up. Inner lining. Inner pocket. Back length 95.0
+                cm.<br>
+                Summer tops for women<br>
+                Cheetah kimonos ,beach cover ups<br>
+                Lightweight chiffon casual shirts<br>
+                Open front outwear,short sleeve blouse.<br>
+            </p>
+            <div class="sku">
+                SKU:<br>
+                FUW237-1<br>
+                Categories: Accessories, Fashion, Mens<br>
+                Tag: teapot<br>
+            </div>
+        `)
+    }
 
 });
--- js/owlcarousel.js
+++ js/owlcarousel.js
@@ -10,12 +10,12 @@ $(document).ready(function () {
         navText: [
             `<i class="fas fa-chevron-left"></i>`,
             `<i class="fas fa-chevron-right"></i>`
-        ]
+        ],
     });
 
     // This is slider list products
     $(".listproduct").owlCarousel({
-        loop: false,
+        loop: true,
         nav: false,
         margin: 20,
         responsive: {
@@ -29,6 +29,13 @@ $(document).ready(function () {
         },
     });
 
+    $("#quickviewSl").owlCarousel({
+        loop: false,
+        nav: false,
+        margin: 20,
+        items: 1,
+        dots: false
+    });
 
     // brand
     $("#brand").owlCarousel({
@@ -80,4 +87,245 @@ $(document).ready(function () {
 
     });
 
-});
+    renderOwl(products, $('#products1'))
+    renderOwl(bestSale, $('#products2'))
+    renderOwlBlog(blogs, $('#blog'))
+})
+
+// Declare valiable of products or blogs post
+var products = [{
+    id: 1,
+    name: 'Women’s classic glasses',
+    price: '80.00',
+    img: '../img/product/20-copy-600x745.jpg',
+    img1: '../img/product/20_1-copy-600x745.jpg',
+    isSale: false,
+    priceSale: '$',
+    isbest: true,
+}, {
+    id: 2,
+    name: 'Daily Ritual Women’s',
+    price: '80.00',
+    img: '../img/product/3_1-600x745.jpg',
+    img1: '../img/product/2_4-copy-600x745.jpg',
+    isSale: true,
+    priceSale: '$65.00',
+    isbest: false,
+}, {
+    id: 3,
+    name: 'Cotton Fleece Jogging',
+    price: '39.00',
+    img: '../img/product/10_1-copy-600x745.jpg',
+    img1: '../img/product/10_2-copy-600x745.jpg',
+    isSale: false,
+    priceSale: '$',
+    isbest: false,
+}, {
+    id: 4,
+    name: 'Sweeper and Funnel',
+    price: '80.00',
+    img: '../img/product/9-copy-600x745.jpg',
+    img1: '../img/product/9_2-copy-1-600x745.jpg',
+    isSale: false,
+    priceSale: '$',
+    isbest: false,
+}, {
+    id: 5,
+    name: 'Unisex Fashion Show',
+    price: '39.00',
+    img: '../img/product/1_1-600x745.jpg',
+    img1: '../img/product/2_4-copy-600x745.jpg',
+    isSale: false,
+    priceSale: '$',
+    isbest: false,
+}, {
+    id: 6,
+    name: 'Mogens Koch Bookcase',
+    price: '28.00',
+    img: '../img/product/8_1-copy-600x745.jpg',
+    img1: '../img/product/8_2-copy-600x745.jpg',
+    isSale: false,
+    priceSale: '$',
+    isbest: true,
+}, {
+    id: 7,
+    name: 'Neocroc backpack in canvas',
+    price: '259.00',
+    img: '../img/product/17_1-copy-600x745.jpg',
+    img1: '../img/product/17-copy-600x745.jpg',
+    isSale: true,
+    priceSale: '$220.00',
+    isbest: false,
+}, {
+    id: 8,
+    name: 'Small Zip Tote Bag',
+    price: '80.00',
+    img: '../img/product/11_1-copy-600x745.jpg',
+    img1: '',
+    isSale: false,
+    priceSale: '$',
+    isbest: false,
+}, {
+    id: 9,
+    name: 'Casual Embossed Lettering',
+    price: '79.00',
+    img: '../img/product/19_1-copy-600x745.jpg',
+    img1: '../img/product/19_2-copy-600x745.jpg',
+    isSale: false,
+    priceSale: '$',
+    isbest: true,
+}, {
+    id: 10,
+    name: 'Checked Cotton Shirt',
+    price: '79.00',
+    img: '../img/product/12-copy-600x745.jpg',
+    img1: '../img/product/12_2-copy-600x745.jpg',
+    isSale: false,
+    priceSale: '$',
+    isbest: true,
+}, {
+    id: 11,
+    name: 'Menerva Elite Textile',
+    price: '79.00',
+    img: '../img/product/22_1-copy-600x745.jpg',
+    img1: '../img/product/21-copy-600x745.jpg',
+    isSale: false,
+    priceSale: '$',
+    isbest: true,
+}, {
+    id: 12,
+    name: 'Mogens Koch Bookca',
+    price: '28.00',
+    img: '../img/product/7_1-copy-600x745.jpg',
+    img1: '../img/product/7-copy-600x745.jpg',
+    isSale: false,
+    priceSale: '',
+    isbest: true,
+}, {
+    id: 13,
+    name: 'Daily Ritual Women’s',
+    price: '220.00',
+    img: '../img/product/2_1-copy-600x745.jpg',
+    img1: '../img/product/2_3-copy-600x745.jpg',
+    isSale: true,
+    priceSale: '$',
+    isbest: true,
+}, {
+    id: 14,
+    name: 'Fashion Show Edition',
+    price: '39.00',
+    img: '../img/product/6_1-copy-600x745.jpg',
+    img1: '',
+    isSale: false,
+    priceSale: '$',
+    isbest: true,
+}]
+const blogs = [{
+    id: "b1",
+    img: '../img/banner/8-500x321.jpg',
+    head: 'Note to wear well in the workplace',
+    des: 'A being too greedy for accessories to Ms. B who looks too old because of the carefulness and dullness of …',
+}, {
+    id: "b2",
+    img: '../img/banner/7-500x321.jpg',
+    head: '5 ways coordination for puff sleeves',
+    des: 'You are a girl who loves gentle, feminine style, “dirt bread” and you love gentle pastel tones and feminine designs. …'
+}, {
+    id: "b3",
+    img: '../img/banner/6-500x321.jpg',
+    head: 'How to mix with tunic shoulders',
+    des: 'How do you wear a shirt on your shoulders every day? Or slip off and maybe not comfortable with that …'
+}, {
+    id: "b4",
+    img: '../img/banner/5-500x321.jpg',
+    head: '7 ways to wear the shoulder blouse',
+    des: 'Shorts, skirts, earrings, hats, … are one of the ways to wear a shoulder- length shirt that any girl can …'
+}]
+const bestSale = products.filter(val => val.isbest)
+
+// Function render a lot of things 
+function renderOwl(datas, owl) {
+
+    datas.map((val, index) => {
+        if (index % 2 != 1 && index < 8) {
+            owl.trigger('add.owl.carousel', [`
+                <div class="item">
+                    <div class="product" data-id = ${val.id}>
+                        <div class="img">
+                            <a href="#">
+                                <img src="${val.img}" alt="">
+                                <img src="${val.img1}" alt="">
+                            </a>
+                            <button class="btn a-center d-flex j-between addcartitem" data-id = ${val.id} >
+                                <i class="bi bi-handbag"></i> Add To Card 
+                            </button>
+                            <ul class="action">
+                                <li class="wishlist"><i class="far fa-heart"></i><span>Add to Wishlist</span>
+                                </li>
+                                <li class="compare"><i class="fas fa-sliders-h"></i> <span>Compare</span> </li>
+                                <li class="detail"><i class="fas fa-eye"></i><span>View Details</span></li>
+                            </ul>
+                        </div>
+
+                        <div class="content-pro">
+                            <h4>${val.name}</h4>
+                            <div class="price">
+                                $${val.price}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="product" data-id = ${datas[index + 1].id}>
+                        <div class="img">
+                            <a href="#">
+                                <img src="${datas[index + 1].img}" alt="">
+                                <img src="${datas[index + 1].img1}" alt="">
+                            </a>
+                            <button class="btn a-center d-flex j-between addcartitem" data-id = ${datas[index + 1].id} >
+                                <i class="bi bi-handbag"></i> Add To Card
+                            </button>
+                            <ul class="action">
+                                <li class="wishlist"><i class="far fa-heart"></i><span> Add to Wishlist</span></li>
+                                <li class="compare"><i class="fas fa-sliders-h"></i> <span>Compare</span> </li>
+                                <li class="detail"><i class="fas fa-eye"></i><span>View Details</span></li>
+                            </ul>
+                        </div>
+
+                        <div class="content-pro">
+                            <h4>${datas[index + 1].name}</h4>
+                            <div class="price">
+                                $${datas[index + 1].price}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `]).trigger("refresh.owl.carousel")
+        }
+    })
+}
+
+function renderOwlBlog(datas, owl) {
+    datas.map((val, index) => {
+        owl.trigger('add.owl.carousel', [`
+            <div class="owl-item">
+                <div class="item">
+                    <a href="#" class="link">
+                        <div class="img">
+                            <img src="${val.img}" alt="">
+                        </div>
+                        <p>Admin. <span class="date">June 10, 2021</span></p>
+                        <h2 class="heading">${val.head}</h2>
+                    </a>
+                    <p class="des">${val.des}</p>
+                    <a href="#" class="btn">Read More</a>
+                </div>
+            </div>
+        `]).trigger("refresh.owl.carousel")
+    })
+}
+
+function getAllItemProduct() {
+
+    return products
+
+}
